f3b30ebd8b1b65134506c88d052f304a04f9be18
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-23 14:01:07 +0200

    add text sebastian branch

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the master branch");
+        System.out.println("This is the Sebastian branch");
+        
 
     }
 }
