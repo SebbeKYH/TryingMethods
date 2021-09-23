3ed1e07859865dd11dffa843caba74458083c6af
Victor S <victor.skarerverket@gmail.com>
2021-09-23 14:52:08 +0200

    Should Now be Colaborator

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is the victor branch");
         System.out.println("Victor checking in!");
+        System.out.println("Do i have push access to the SebbeKYH/TryingMethods ?")
     }
 }
