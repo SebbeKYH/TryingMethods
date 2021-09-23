8c21785b624d9f331cb69dd564b376878615a14f
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-23 18:42:57 +0200

    testing methods

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the Sebastian branch");
-        
 
-    }
+        Scanner input = new Scanner(System.in);
+
+        int firstNumber=input.nextInt();
+        int divided=divideMetod(firstNumber);
+        System.out.println(divided);
+        }
+
+    public static int divideMetod (int firstNumber){
+        int results =(firstNumber/2);
+        return results;
+        }
 }
+
+
