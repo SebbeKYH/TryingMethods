2b7bace36c9e0995fe7de0d2391fb20ff8532690
Victor S <victor.skarerverket@gmail.com>
2021-09-23 14:21:01 +0200

    Victors initial commit

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the victor branch");
-
+        System.out.println("Victor checking in!");
     }
 }
