77be2ee5af1fcd987bb94ae11322950087f063cd
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-23 14:13:57 +0200

    added code

--- com/company/Main.java
+++ com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the master branch");
+        System.out.println("This is the victor branch");
 
     }
 }
