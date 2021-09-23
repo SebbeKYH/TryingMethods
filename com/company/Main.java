241bf9ced01470a5b173e8d7906429624f1d2059
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-23 14:11:00 +0200

    change code

--- com/company/Main.java
+++ com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the master branch");
         System.out.println("This is the Sebastian branch");
         
 
