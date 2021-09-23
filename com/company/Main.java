a6cc3c9d6638682d17ec0df8ed82538a3e0210ce
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2021-09-23 14:32:17 +0200

    Gösta uppdaterar sin  branch

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the gösta branch");
+        System.out.println("Ändrar på Gösta branchen");
 
     }
 }
